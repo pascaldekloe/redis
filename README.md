cda7d8d4d39d89a41ebafc33948e11ca3c006cb8
Pascal S. de Kloe <pascal@quies.net>
2019-09-05 15:32:59 +0200

    Toy reget(8).

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## About
 
-… a redis client library for the Go programming language.
+… a redis client for the Go programming language.
 
 This is free and unencumbered software released into the
 [public domain](https://creativecommons.org/publicdomain/zero/1.0).
---
+++ cmd/reget/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/pascaldekloe/redis"
+)
+
+var (
+	addrFlag = flag.String("addr", "localhost:6379", "Redis service address.")
+	rawFlag  = flag.Bool("raw", false, "Prints the value as is, instead of a quoted string plus line feed.")
+)
+
+var Redis *redis.Client
+
+func main() {
+	flag.Parse()
+
+	Redis = redis.NewClient(*addrFlag, 0, 0)
+
+	print(flag.Args())
+}
+
+func print(keys []string) {
+	for _, key := range keys {
+		value, err := Redis.GET(key)
+		switch {
+		case err != nil:
+			os.Stderr.WriteString(err.Error())
+			os.Exit(255)
+		case *rawFlag:
+			os.Stdout.Write(value)
+		default:
+			fmt.Printf("%q\n", value)
+		}
+	}
+}
