b7a1ffe98b0b386e1f3678c313392284e5a0bb17
Pascal S. de Kloe <pascal@quies.net>
2022-04-18 18:46:51 +0200

    New auth, delimit, terminate and null flags for reget(1).

--- cmd/reget/main.go
+++ cmd/reget/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
 
 	"github.com/pascaldekloe/redis"
 )
 
 var (
-	addrFlag = flag.String("addr", "localhost:6379", "Redis service address.")
-	rawFlag  = flag.Bool("raw", false, "Prints the value as is, instead of a quoted string plus line feed.")
+	addrFlag = flag.String("addr", "localhost:6379", "Redis node `address`.")
+	authFlag = flag.Bool("auth", false, "Reads a password from the standard input.")
+
+	rawFlag       = flag.Bool("raw", false, "Output values as is, instead of quoted strings.")
+	delimitFlag   = flag.String("delimit", "\n", "The output `separator` between values.")
+	terminateFlag = flag.String("terminate", "\n", "The output `suffix` on the last value.")
+	nullFlag      = flag.String("null", "<null>", "The output `value` for key absence.")
 )
 
 // Redis manages the connection.
@@ -18,24 +25,60 @@ var Redis *redis.Client
 
 func main() {
 	flag.Parse()
+	keys := flag.Args()
+	if len(keys) == 0 {
+		os.Stderr.WriteString(`NAME
+	reget — resolve Redis content
+
+SYNOPSIS
+	reget [ options ] [ key ... ]
+
+DESCRIPTION
+	For each operand, reget prints the associated value according to
+	the node.
+
+	The following options are available:
+
+`)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
 	Redis = redis.NewClient(*addrFlag, 0, 0)
 	defer Redis.Close()
+	if *authFlag {
+		password, _ := ioutil.ReadAll(os.Stdin)
+		if err := Redis.AUTH(password); err != nil {
+			fmt.Fprintln(os.Stderr, "reget: AUTH with", err)
+			os.Exit(4)
+		}
+	}
 
-	print(flag.Args())
+	print(keys)
 }
 
 func print(keys []string) {
-	for _, key := range keys {
-		value, err := Redis.GET(key)
+	values, err := Redis.MGET(keys...)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "reget: MGET with", err)
+		os.Exit(255)
+	}
+
+	w := os.Stdout
+	for i, v := range values {
 		switch {
-		case err != nil:
-			os.Stderr.WriteString(err.Error())
-			os.Exit(255)
+		case v == nil:
+			w.WriteString(*nullFlag)
 		case *rawFlag:
-			os.Stdout.Write(value)
+			w.Write(v)
 		default:
-			fmt.Printf("%q\n", value)
+			w.WriteString(strconv.QuoteToGraphic(string(v)))
+		}
+
+		if i < len(values)-1 {
+			w.WriteString(*delimitFlag)
+		} else {
+			w.WriteString(*terminateFlag)
 		}
 	}
 }
